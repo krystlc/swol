eb71d81e19d5f2f2a796bd3aa2cf2c8fa2c9664b
krystlc <alex.valle@gmail.com>
2018-03-21 18:59:31 -0400

    added exercise.json list

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,67 @@
 <template>
   <div id="app">
     <h1>SWOL</h1>
-    <hr>
-    <h3>Sessions</h3>
     <form @submit.prevent="handleSubmit" action="#" method="post">
-      <select v-model="workout.name">
-        <option>Push ups</option>
-        <option>Pull ups</option>
-        <option>Chin ups</option>
-        <option>Stand ups</option>
-      </select>
-      <input type="number" v-model.number="workout.weight">
-      <input type="number" v-model.number="workout.sets">
-      <input type="number" v-model.number="workout.reps">
-      <button type="submit">Save Workout</button>
+      <fieldset>
+        <select v-model="exercise">
+          <option disabled selected value>Select workout</option>
+          <option v-for="(exName, index) in $options.exList" :key="index">{{ exName }}</option>
+        </select>
+        <label>Weight</label>
+        <input type="number" v-model.number="weight" min="0" step="5">
+        <label>Sets</label>
+        <input type="number" v-model.number="sets" min="1">
+        <label>Reps</label>
+        <input type="number" v-model.number="reps" min="1">
+        <button type="submit">Add Workout</button>
+      </fieldset>
     </form>
+    <h3>Workout session</h3>
+    <template v-if="sessions.length > 0">
+      <div v-for="(workout, index) in sessions" :key="index">
+        <span>{{ workout.name }} - {{ workout.weight }}lbs, {{ workout.sets }} sets and {{ workout.reps }} reps</span>
+      </div>
+      <h5>Nice, keep going!</h5>
+    </template>
+    <template v-else>
+      <h5>What a loser, you haven't done shit.</h5>
+    </template>
   </div>
 </template>
 
 <script>
+import exerciseData from './assets/exercises.json'
 
 export default {
   name: 'App',
+  exList: exerciseData.exercises,
   data () {
     return {
-      workout: {
-        name: '',
-        weight: null,
-        sets: null,
-        reps: null
-      },
+      exercise: '',
+      weight: null,
+      sets: null,
+      reps: null,
       sessions: []
     }
   },
   methods: {
-    handleSubmit: () => {
-      this.sessions.push({name: this.name})
-      console.log(this.sessions)
+    handleSubmit: function () {
+      this.sessions.push({
+        exercise: this.exercise,
+        weight: this.weight,
+        sets: this.sets,
+        reps: this.reps
+      })
     }
   }
 }
 </script>
 
 <style scoped>
+#app {
+  padding: 2em
+}
 input {
-  width: 60px
+  width: 80px
 }
 </style>
