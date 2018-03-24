be1e11f5d266382ea8db9bfba46ab0c3acdf91fb
krystlc <alex.valle@gmail.com>
2018-03-24 12:07:23 -0400

    fixed sync bug

--- src/components/WorkoutForm.vue
+++ src/components/WorkoutForm.vue
@@ -1,13 +1,12 @@
 <template>
   <form @submit.prevent="handleSubmit" action="#" method="post">
     <b-field grouped group-multiline>
-      <b-select v-model="workout.exercise" required>
-        <option disabled>Select exercise</option>
+      <b-select v-model="exercise" required>
         <option v-for="(exName, index) in $options.exList" :key="index">{{ exName }}</option>
       </b-select>
-      <b-input type="number" v-model.number="workout.weight" min="0" step="5"></b-input>
-      <b-input type="number" v-model.number="workout.sets" min="1" required></b-input>
-      <b-input type="number" v-model.number="workout.reps" min="1" required></b-input>
+      <b-input type="number" v-model.number="weight" min="0" step="5"></b-input>
+      <b-input type="number" v-model.number="sets" min="1" required></b-input>
+      <b-input type="number" v-model.number="reps" min="1" required></b-input>
       <p class="control">
         <button type="submit" class="button is-primary">Add Workout</button>
       </p>
@@ -23,17 +22,20 @@ export default {
   exList: exerciseData.exercises,
   data () {
     return {
-      workout: {
-        exercise: '',
-        weight: null,
-        sets: 3,
-        reps: 10
-      }
+      exercise: '',
+      weight: null,
+      sets: 3,
+      reps: 10
     }
   },
   methods: {
     handleSubmit: function () {
-      this.$emit('workout', this.workout)
+      this.$emit('workout', {
+        exercise: this.exercise,
+        weight: this.weight,
+        sets: this.sets,
+        reps: this.reps
+      })
       this.weight = null
     }
   }
