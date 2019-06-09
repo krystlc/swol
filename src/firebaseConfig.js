1471f90ec2155b9b1b59bb4919f64bd8c7ad5a21
Alex Valle <alex.valle@gmail.com>
2019-06-09 16:28:10 -0400

    removed deprecated firebase setting

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,7 +7,7 @@ const config = {
   apiKey: process.env.VUE_APP_API_KEY,
   authDomain: process.env.VUE_APP_DOMAIN,
   databaseURL: process.env.VUE_APP_DATABASE,
-  projectId: process.env.VUE_APP_PROJECT_ID,
+  projectId: process.env.VUE_APP_PROJECT_ID
 }
 firebase.initializeApp(config)
 
@@ -18,12 +18,6 @@ const fieldValue = firebase.firestore.FieldValue
 const auth = firebase.auth()
 const provider = new firebase.auth.GoogleAuthProvider()
 const currentUser = auth.currentUser
-
-// date issue fix according to firebase
-const settings = {
-  timestampsInSnapshots: true
-}
-db.settings(settings)
 
 // firebase collections
 const userCollection = db.collection('users')
