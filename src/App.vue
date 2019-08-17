2b8bc2094c07b51ccaeacda02527eeebf3bef63a
Alex Valle <alex.valle@gmail.com>
2019-08-17 12:14:34 -0400

    removed navbar by default

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <template>
   <div id="app">
-    <navbar/>
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
-
-<script>
-import Navbar from '@/components/Navbar'
-
-export default {
-  components: { Navbar }
-}
-</script>
