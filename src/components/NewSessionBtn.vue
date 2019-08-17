eaa42ed95686873f666e422116f7326689f182d7
Alex Valle <alex.valle@gmail.com>
2019-08-17 16:24:10 -0400

    created a button component to create sessions

---
+++ src/components/NewSessionBtn.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="fab">
+    <b-tooltip
+      label="Start a new session!"
+      position="is-left"
+      animated
+    >
+      <router-link class="button is-primary" to="/s/new" tag="button">
+        <b-icon icon="plus" size="is-medium"></b-icon>
+      </router-link>
+    </b-tooltip>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.fab {
+  position: fixed;
+  bottom: 2rem;
+  right: 2rem;
+
+  .button {
+    width: 4rem;
+    height: 4rem;
+    border-radius: 50%;
+  }
+}
+</style>
