ce0d8ff35551817d6eafe4f90f4c93b2a0cc76db
Alex Valle <alex.valle@gmail.com>
2019-08-17 16:24:34 -0400

    get latest session

--- src/store/modules/sessionCollection.js
+++ src/store/modules/sessionCollection.js
@@ -6,6 +6,7 @@ export default {
   namespaced: true,
   getters: {
     list: state => state.data,
-    getSession: state => id => state.data[id]
+    getSession: state => id => state.data[id],
+    latest: state => state.data ? state.data[Object.keys(state.data)[0]] : null
   },
 }
