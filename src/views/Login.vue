043c2b23ed5b1dc2b1b82da6c7bf23ea65daaefa
Alex Valle <alex.valle@gmail.com>
2019-08-17 16:23:17 -0400

    created a splash layout without a navbar

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,21 +1,26 @@
 <template>
-  <section class="hero is-black is-fullheight">
-    <div class="hero-body">
-      <div class="container has-text-centered">
-        <h1 class="title">Get SWOL</h1>
-        <h2 class="subtitle">and log those gains</h2>
-        <button class="button is-primary is-outlined is-large" @click.prevent="signIn">
-          <span>Sign in</span>
-        </button>
+  <splash-layout>
+    <section class="hero is-black is-fullheight">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">Get SWOL</h1>
+          <h2 class="subtitle">and log those gains</h2>
+          <button class="button is-primary is-outlined is-large" @click.prevent="signIn">
+            <span>Sign in</span>
+          </button>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </splash-layout>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
 
 export default {
+  components: {
+    SplashLayout: () => import("@/layouts/SplashLayout")
+  },
   mounted() {
     if (this.getUserId) this.$router.push("/dashboard");
   },
